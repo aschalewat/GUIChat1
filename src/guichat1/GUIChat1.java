56503e61e0d3369eb05afcbe7bed107880088a87
Aschalew A. Tessema <aschalewat@yahoo.co.uk>
2017-01-12 23:22:52 +0000

    First development commitwww

--- src/guichat1/GUIChat1.java
+++ src/guichat1/GUIChat1.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 import java.io.*;
 import java.net.*;
 
-//edited today
+//edited todayhghh
 /**
  * Opens a window that can be used for a two-way network chat.
  * The window can "listen" for a connection request on a port
